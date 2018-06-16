763355bdf23cb7d02496ad2e1b86545167f7d124
Valerie Moore <periwinkle17@outlook.com>
2018-06-16 23:28:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  #binding.pry
+  binding.pry
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies| supplies.include?("BBQ")
     end
